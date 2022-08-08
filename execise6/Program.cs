8ef3a840621adcbc16054a344a6985bd29ed93ed
unknown <artem03051987@gmail.com>
2022-08-09 01:25:23 +0500

    показать вторую цифру трёхзначного числа

---
+++ execise6/Program.cs
@@ -0,0 +1,7 @@
+﻿// 6. Показать вторую цифру трёхзначного числа
+
+Console.Clear();
+System.Console.WriteLine();
+int number = 737;
+System.Console.WriteLine($"Второй цифрой числа {number} является {number / 10 % 10} ");
+System.Console.WriteLine(); 
