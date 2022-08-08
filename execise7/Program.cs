52dca3293d35ffa35eed50d436aea090fa8a2493
unknown <artem03051987@gmail.com>
2022-08-09 01:31:55 +0500

    дано число из отрезка [10,99].Показать наибольшую цифру числа

---
+++ execise7/Program.cs
@@ -0,0 +1,14 @@
+﻿// 7. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Console.Clear();
+
+int a = new Random().Next(10, 100);
+int Max = 0;
+
+int MaxDigit()
+{
+    int One = a / 10;
+    int Two = a % 10;
+    if (One > Two) Max = One;
+    else Max = Two;
+}
