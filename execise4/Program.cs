191e5793ec4fb9addb9d04da8e6ed958dc87c5fa
unknown <artem03051987@gmail.com>
2022-08-09 01:13:31 +0500

    выяснить является ли число четным

---
+++ execise4/Program.cs
@@ -0,0 +1,8 @@
+﻿//4 Выяснить является ли число четным.
+int x = 6; 
+{
+        if (x % 2 == 0)
+              Console.WriteLine(" += 1"); 
+        else
+               Console.WriteLine(" -= 1"); 
+}
