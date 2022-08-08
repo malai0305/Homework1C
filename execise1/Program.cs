d0147a5ac4304a9a48a9d8d28f7a0ec195520274
unknown <artem03051987@gmail.com>
2022-08-09 00:17:37 +0500

    по двум заданным числам проверить является ли первое квадратом второго

---
+++ execise1/Program.cs
@@ -0,0 +1,11 @@
+﻿// По двум заданным числам проверить является ли первое квадратом второго.
+int a = 2;
+int b = 4;
+if (a*a==b)
+{
+    Console.WriteLine("Yes");
+}
+else if (a*a>b) if (a*a<b)
+{
+    Console.WriteLine("No");
+}
